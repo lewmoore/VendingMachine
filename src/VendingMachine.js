697e82a5aa80ec96a56beb6b3df0a1e8a703b2b2
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-17 16:05:47 +0100

    Change itemList to object and amend tests

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,5 +1,5 @@
 function VendingMachine(){
-  this.itemList = ['Kit-Kat', 'Coca-Cola', 'Crisps']
+  this.itemList = {'Kit-Kat': 1.50, 'Coca-Cola': 3.60, 'Crisps': 2.75}
 }
 
 VendingMachine.prototype.viewItems = function(){
--- test/vendingMachineTest.js
+++ test/vendingMachineTest.js
@@ -2,10 +2,10 @@ describe('VendingMachine', function(){
   var vendingMachine = new VendingMachine
 
   it('is initialised with a list of items', function(){
-    expect(vendingMachine.itemList).toEqual(['Kit-Kat', 'Coca-Cola', 'Crisps'])
+    expect(vendingMachine.itemList).toEqual({'Kit-Kat': 1.50, 'Coca-Cola': 3.60, 'Crisps': 2.75})
   })
 
   it('can view the list of items in the vending machine', function(){
-    expect(vendingMachine.viewItems()).toEqual(['Kit-Kat', 'Coca-Cola', 'Crisps'])
+    expect(vendingMachine.viewItems()).toEqual({'Kit-Kat': 1.50, 'Coca-Cola': 3.60, 'Crisps': 2.75})
   })
 })
