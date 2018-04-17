6213090132a061473b061bea7fe8b8162c52ad97
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-17 16:00:13 +0100

    Add function to return itemList

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,3 +1,7 @@
 function VendingMachine(){
   this.itemList = ['Kit-Kat', 'Coca-Cola', 'Crisps']
 }
+
+VendingMachine.prototype.viewItems = function(){
+  return this.itemList
+}
--- test/vendingMachineTest.js
+++ test/vendingMachineTest.js
@@ -1,7 +1,11 @@
-describe('vendingMachine', function(){
+describe('VendingMachine', function(){
   var vendingMachine = new VendingMachine
 
   it('is initialised with a list of items', function(){
     expect(vendingMachine.itemList).toEqual(['Kit-Kat', 'Coca-Cola', 'Crisps'])
+  })
+
+  it('can view the list of items in the vending machine', function(){
+    expect(vendingMachine.viewItems()).toEqual(['Kit-Kat', 'Coca-Cola', 'Crisps'])
   })
 })
